81f7d3dbd7f6f51275f546d29a98e94435cbcb20
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-06-02 11:20:12 +0200

    using bloc auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
+import 'package:mynotes/services/auth/firebase_auth_provider.dart';
 import 'package:mynotes/views/Register_view.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notes/create_update_note_view.dart';
 import 'package:mynotes/views/notes/note_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
-import 'dart:developer' as devtool show log;
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(primarySwatch: Colors.green),
-    home: const Homepage(),
+    home: BlocProvider(
+      create: (context) => AuthBloc(FirebaseAuthProvider()),
+      child: const Homepage(),
+    ),
     routes: {
       loginRoute: (context) => const LoginView(),
       registerRoute: (context) => const RegisterView(),
@@ -26,168 +31,24 @@ void main() {
   ));
 }
 
-// class Homepage extends StatelessWidget {
-//   const Homepage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return FutureBuilder(
-//       future: AuthService.firebase().initialize(),
-//       builder: (context, snapshot) {
-//         switch (snapshot.connectionState) {
-//           case ConnectionState.done:
-//             final user = AuthService.firebase().currentUser;
-//             if (user != null) {
-//               if (user.isEmailVerified) {
-//                 devtool.log('Email is verified');
-//                 return const NoteView();
-//               } else {
-//                 return const VerifiedEmailView();
-//               }
-//             } else {
-//               devtool.log('user is null');
-//               return const LoginView();
-//             }
-
-//           default:
-//             return const CircularProgressIndicator();
-//         }
-//       },
-//     );
-//   }
-// }
-class Homepage extends StatefulWidget {
+class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
 
   @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  late final TextEditingController _controller;
-
-  @override
-  void initState() {
-    _controller = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Testing Bloc'),
-        ),
-        body: BlocConsumer<CounterBloc, CounterState>(
-          listener: (context, state) {
-            _controller.clear();
-          },
-          builder: (context, state) {
-            final invalidValue =
-                (state is CounterStateInvalidNumber) ? state.invalidValue : '';
-            return Column(children: [
-              Text('Current value => ${state.value}'),
-              Visibility(
-                child: Text('Invalid input: $invalidValue'),
-                visible: state is CounterStateInvalidNumber,
-              ),
-              TextField(
-                controller: _controller,
-                decoration:
-                    const InputDecoration(hintText: 'Enter a number here'),
-                keyboardType: TextInputType.number,
-              ),
-              Row(
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        context.read<CounterBloc>().add(DecrementEvent(_controller.text));
-                      },
-                      child: const Text('-')),
-                  TextButton(onPressed: () {
-                    context
-                            .read<CounterBloc>()
-                            .add(IncrementEvent(_controller.text));
-                  }, child: const Text('+')),
-                ],
-              )
-            ]);
-          },
-        ),
-      ),
-    );
-  }
-}
-
-@immutable
-abstract class CounterState {
-  final int value;
-  const CounterState(this.value);
-}
-
-class CounterStateValid extends CounterState {
-  const CounterStateValid(int value) : super(value);
-}
-
-class CounterStateInvalidNumber extends CounterState {
-  final String invalidValue;
-
-  const CounterStateInvalidNumber({
-    required this.invalidValue,
-    required int previousValue,
-  }) : super(previousValue);
-}
-
-@immutable
-abstract class CounterEvent {
-  final String value;
-
-  const CounterEvent(this.value);
-}
-
-class IncrementEvent extends CounterEvent {
-  const IncrementEvent(String value) : super(value);
-}
-
-class DecrementEvent extends CounterEvent {
-  const DecrementEvent(String value) : super(value);
-}
-
-class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(const CounterStateValid(0)) {
-    on<IncrementEvent>(((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(CounterStateInvalidNumber(
-          invalidValue: event.value,
-          previousValue: state.value,
-        ));
+    context.read<AuthBloc>().add(const AuthEventInitialize());
+    return BlocBuilder<AuthBloc, AuthState>(builder: (context, state) {
+      if (state is AuthStateLoggedIn) {
+        return const NoteView();
+      } else if (state is AuthStateNeedsVerification) {
+        return const VerifiedEmailView();
+      } else if (state is AuthStateLoggedOut) {
+        return const LoginView();
       } else {
-        emit(
-          CounterStateValid(state.value + integer),
+        return const Scaffold(
+          body: CircularProgressIndicator(),
         );
       }
-    }));
-    on<DecrementEvent>(((event, emit) {
-      final integer = int.tryParse(event.value);
-      if (integer == null) {
-        emit(CounterStateInvalidNumber(
-          invalidValue: event.value,
-          previousValue: state.value,
-        ));
-      } else {
-        emit(
-          CounterStateValid(state.value - integer),
-        );
-      }
-    }));
+    });
   }
 }
